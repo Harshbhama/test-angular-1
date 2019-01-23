6a0125a70fb01a702f69229b499d3bf38589d325
unknown <harshbhama96@gmail.com>
2019-01-23 16:38:07 +0530

    add data binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,10 +2,20 @@ import {Component} from '@angular/core';
 
 @Component({
 
+   // selector: '.app-server'
     selector: 'app-server',
     templateUrl: './server.component.html'
 
 })
 export class ServerComponent{
 
+    serverid = 10
+    serverstatus = 'offline'
+    servertest = 'testmethod'
+
+    getservertestthis()
+    {
+        return this.servertest;
+    }
+    
 }
